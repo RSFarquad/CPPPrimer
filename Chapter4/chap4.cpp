cf8c67499623c0737e528ced15d059b7e079da08
Rylan Sharrett <rylmitchell@gmail.com>
2020-09-14 15:17:33 -0400

    9/14

---
+++ Chapter4/chap4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using std::vector;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::begin;
+using std::end;
+
+/*
+//p.136
+int main() {
+	int n1 = 5 + 10 * 20 / 2; //105
+	cout << n1 << "\n";
+	return 0;
+	
+	// (* (vec.begin()))
+	// ((* (vec.begin())) + 1)
+
+}
+*/
+
