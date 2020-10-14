64fd00374027abcf1da2cf368cb728b28fc47eec
Rylan Sharrett <rylmitchell@gmail.com>
2020-10-14 10:29:35 -0400

    Chapter 6 start.

---
+++ Chapter6/chap6.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stdexcept>
+#include <exception>
+using std::vector;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::begin;
+using std::end;
+using std::getline;
+
